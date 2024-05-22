407f203739bb3a30960bf6aeef3019c95742e8a4
Jorge L. Batista <jorgeluisbatista99@gmai.com>
2024-05-22 14:59:17 -0600

    Añadiendo parte de los usuarios TODO

--- todor/todo.py
+++ todor/todo.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('todo', __name__, url_prefix='/todo')
 
 
 @bp.route('/list')
 def lista():
-    return 'Lista de tareas'
+    return render_template('todo/index.html')
 
 
 @bp.route('/create')
